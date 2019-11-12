ce095464f8df16e988e0b872c0db3c78c2ee1760
Franklin Tarter <franklintarter@gmail.com>
2019-11-12 08:07:24 -0800

    adds window check for ssr support

--- src/createHook.js
+++ src/createHook.js
@@ -2,6 +2,12 @@
 import { useState } from "react";
 import useEvent from "./useEvent";
 
+let initialYOffset = 0;
+
+if (typeof window !== `undefined`) {
+  initialYOffset = window.pageYOffset;
+}
+
 export default ({ height = 48 }) => () => {
   const [isOpen, setOpen] = useState(false);
 
@@ -19,7 +25,7 @@ export default ({ height = 48 }) => () => {
 
   const [isScrolled, setScrolled] = useState(false);
 
-  const [lastScroll, setLastScroll] = useState(window.pageYOffset);
+  const [lastScroll, setLastScroll] = useState(initialYOffset);
   const handler = () => {
     const st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScroll) {
